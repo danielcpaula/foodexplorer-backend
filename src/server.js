3179de0176a031fa4f1df359f296f616badae4b8
Daniel do Casal de Paula <daniel.c.paula@hotmail.com>
2022-08-08 11:32:10 -0300

    creation of error handling

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("express-async-errors")
 require("dotenv/config")
+const AppError = require("./util/appError")
 
 
 const cors = require("cors")
@@ -12,7 +13,21 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
 
+  console.error(error)
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 
 const port = 3333
